a5cdce7a0516edb08b682ccfd1622c2828adb0e2
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-10-05 21:01:08 -0500

    en arreglos de numpy, falta plotear

--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Scale:
     """
     Rescale numbers, see D3js scale object.
@@ -22,3 +25,41 @@ class Scale:
             (max(self.range) - min(self.range))) \
             / (max(self.domain) - min(self.domain))
         return mx * (a - min(self.domain)) + min(self.range)
+
+
+def square(x):
+    return x**2
+
+
+def in_set(x):
+    counter = 0
+    a = 0
+    b = 0
+    while (a**2 + b**2) < 4 and counter < 100:
+        aux = a
+        a = a ** 2 - b ** 2 + x.real
+        b = 2 * aux * b + x.imag
+        counter += 1
+    return counter
+
+
+width = 18
+height = 18
+
+xscale = Scale(domain=[0, width - 1], range=[-2, 1])
+yscale = Scale(domain=[0, height - 1], range=[-1, 1])
+
+a = np.zeros((width, height), dtype=complex)
+plot = np.zeros((width, height), dtype=int)
+
+for i in range(width):
+    for j in range(height):
+        a[i][j] = complex(xscale.linear(i), yscale.linear(j))
+
+print a
+
+for i in range(width):
+    for j in range(height):
+        plot[i][j] = in_set(a[i][j])
+
+print plot
