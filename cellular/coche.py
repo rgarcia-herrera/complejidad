9fb9697c0f0a6fad19db6f3728decb6347343885
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-11-17 18:54:16 -0600

    atinados comentarios

--- cellular/coche.py
+++ cellular/coche.py
@@ -1,4 +1,43 @@
+# coding: utf-8
+
 from time import sleep
+
+
+def next_gen(universo, rule):
+    """
+    Genera un nuevo universo a partir del dado
+    usando la regla de evolucion
+    """
+
+    # copiar el universo a una variable interna de la funcion
+    u = universo[:]
+    # repito la primera celda al final para condicion periodica
+    u.append(u[-1])
+
+    v = []  # inicializo nueva generacion
+
+    for n in range(len(u)-1):
+        vecindad = []
+        # construimos la vecindad considerando tres celdas
+        for i in [n - 1,
+                  n,
+                  n + 1]:
+            vecindad.append(u[i])
+        # aplicamos la regla por cada celda y sus vecinos
+        v.append(rule[tuple(vecindad)])
+    return v
+
+
+def to_str(universo):
+    """
+    mejor aspecto para imprimir la lista de unos y ceros
+    """
+    return "".join(['#' if c == '1'
+                    else ' '
+                    for c in universo])
+
+
+g0 = list("010000000000000000000000000000000000000000000000000000")
 
 siga = {
     ('0', '0', '0'): '0',
@@ -11,7 +50,6 @@ siga = {
     ('1', '1', '1'): '1',
 }
 
-
 alto = {
     ('0', '0', '0'): '0',
     ('0', '0', '1'): '0',
@@ -23,30 +61,9 @@ alto = {
     ('1', '1', '1'): '1',
 }
 
+regla = siga  # regla inicial
 
-def next_gen(universo, rule):
-    # copiar el universo
-    u = universo[:]
-    # repito la primera celda al final para condicion periodica
-    u.append(u[-1])
-
-    v = []  # nueva generacion
-
-    for n in range(len(u)-1):
-        vecindad = []
-        for i in [n - 1, n, n + 1]:
-            vecindad.append(u[i])
-        v.append(rule[tuple(vecindad)])
-    return v
-
-
-def to_str(universo):
-    return "".join(['#' if c == '1' else ' '  for c in universo])
-
-
-g0 = list("010000000000000000000000000000000000000000000000000000")
-regla = siga
-
+# simulación de semáforo
 while True:
     try:
         g1 = next_gen(g0, regla)
@@ -54,10 +71,11 @@ while True:
         g0 = g1[:]
         sleep(0.2)
     except KeyboardInterrupt:
+        # para alternar el semáforo entre alto y siga usar Ctrl-C
         if regla == siga:
             regla = alto
             print "alto!"
         else:
             regla = siga
             print "siga!"
-        sleep(0.3)
+        sleep(0.3)  # doble Ctrl-C detiene la simulacion
